174e23d3d6f0c524506a9864c158c317459fbfab
A Shamrock <53099864+ashamrock@users.noreply.github.com>
2019-10-14 18:41:40 -0400

    Add files via upload

--- liri.js
+++ liri.js
@@ -1,58 +1,25 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
-var band = process.argv[2];
-var concert = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp";
-    
-    console.log(queryURL); 
+// var keys = require('./keys.js');
+// var Spotify = require('node-spotify-api');
+var question = process.argv[2];
+// var queryUrl;
+var queryURL = "https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp" + bandName + "/events?app_id=[key]";
 
-    axios.get(queryURL).then(
-        function(data){
-            console.log("City: " + data.data[0].venue.city);
-            console.log("Venue: " + data.data[0].venue.name);
-            console.log(moment(data.data[0].datetime).format("MM/DD/YYYY"));
-        }
-    );
-
-var ask = function (question, response){
     switch(question) {
         case "concert-this":
-            concertIt(response);
+            concertThis(response);
             break;
         case "movie-this" :
-            movieIt(response);
+            movieThis(response);
             break;    
         case 'spotify-this-song':
-            spotifyIt(response); 
+            spotifyThis(response); 
             break;
-        default:
-        console.log("Invalid command. Please try again");
     }
-};
 
 axios.get(queryURL).then(
-    function(data){
-        console.log("Venue: " + data.data[0].venue.name);
-        console.log("City: " + data.data[0].venue.city);
-        console.log(moment(data.data[0].datetime).format("MM/DD/YYYY"));
+        function(bandResponse){
+            console.log(bandResponse.data.venue.name + bandResponse.data.venue.city + bandResponse.data.datetime);
     }
 );
-
-console.log(queryURL); 
-
-if (action === "concert-this" ) {
-    queryUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy"
-  } 
-
-if (action === "movie-this") {
-    queryUrl = "http://www.omdbapi.com/?t=" + mediaName + "&y=&plot=short&apikey=trilogy";
-  } 
-
-if (action === "spotify-this-song") {
-   fs.readFile("movies.txt", "utf8", function(error, data) {
-    console.log(data);
-    var dataArr = data.split(",");
-    console.log(dataArr);
-  });
- }
